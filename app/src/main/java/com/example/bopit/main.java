095223f8dae04062143b1b718ee27741db39e822
Chris <inbox.chrisf@gmail.com>
2018-06-27 23:21:17 -0400

    added logo

--- app/src/main/java/com/example/bopit/main.java
+++ app/src/main/java/com/example/bopit/main.java
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                   *
- * Bopit                                                             *
+ * Bap-it                                                             *
  * Version: Beta                                                     *
  * Author: cjfcode and SentientTowel                                 *
  * Date: 06/26/18                                                    *
@@ -11,11 +11,12 @@
 package com.example.bopit;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class main extends AppCompatActivity {
     @Override
@@ -23,9 +24,11 @@ public class main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView code_mainTitle = findViewById (R.id.v_main_title);
+        ConstraintLayout code_mainLayout = findViewById (R.id.v_main_layout);
+        ImageView code_mainTitle = findViewById (R.id.v_main_title);
         Button code_mainStartBtn = findViewById(R.id.v_main_btn_start);
 
+        /* code within the function below is used to control the actions of the start button */
         code_mainStartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
